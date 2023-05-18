27ae361268294e0316a21d8392ad4fedb27889a8
FarruxDEV <farruxbeknozimov@gmail.com>
2023-05-18 19:17:40 +0500

    vue build

--- src/components/Rightbar/Rightbar.vue
+++ src/components/Rightbar/Rightbar.vue
@@ -2,7 +2,7 @@
 import Avatar from '../../UI/Avatar.vue'
 </script>
 <template lang="">
-  <div class="absolute right-0 top-0 bg-gray-100 h-[100vh] w-[300xp] py-7 px-5">
+  <div class="absolute right-0 top-0 bg-gray-100 h-[100vh] w-72 py-10 px-5">
     <Avatar />
   </div>
 </template>
